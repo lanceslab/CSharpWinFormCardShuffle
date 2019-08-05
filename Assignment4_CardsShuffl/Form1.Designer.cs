e961a37ed0547139392f27acaa9bc1ae6025ecd4
LancesLab <lanceandsheri@yahoo.com>
2019-08-05 14:20:51 -0400

    Project upload

---
+++ Assignment4_CardsShuffl/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Assignment4_CardsShuffl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDeal = new System.Windows.Forms.Button();
+            this.buttonShuffle = new System.Windows.Forms.Button();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonDeal
+            // 
+            this.buttonDeal.Location = new System.Drawing.Point(17, 16);
+            this.buttonDeal.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonDeal.Name = "buttonDeal";
+            this.buttonDeal.Size = new System.Drawing.Size(100, 28);
+            this.buttonDeal.TabIndex = 0;
+            this.buttonDeal.Text = "Deal";
+            this.buttonDeal.UseVisualStyleBackColor = true;
+            this.buttonDeal.Click += new System.EventHandler(this.buttonDeal_Click);
+            // 
+            // buttonShuffle
+            // 
+            this.buttonShuffle.Location = new System.Drawing.Point(125, 15);
+            this.buttonShuffle.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonShuffle.Name = "buttonShuffle";
+            this.buttonShuffle.Size = new System.Drawing.Size(100, 28);
+            this.buttonShuffle.TabIndex = 1;
+            this.buttonShuffle.Text = "Shuffle";
+            this.buttonShuffle.UseVisualStyleBackColor = true;
+            this.buttonShuffle.Click += new System.EventHandler(this.buttonShuffle_Click);
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOutput.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.labelOutput.Location = new System.Drawing.Point(269, 9);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(580, 35);
+            this.labelOutput.TabIndex = 2;
+            this.labelOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 432);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.buttonShuffle);
+            this.Controls.Add(this.buttonDeal);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lance\'sLab - Card Shuffle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDeal;
+        private System.Windows.Forms.Button buttonShuffle;
+        private System.Windows.Forms.Label labelOutput;
+    }
+}
+
---
+++ Assignment4_CardsShuffl/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment4_CardsShuffl
+{
+    public partial class Form1 : Form
+    {
+
+        PlayingCards.Deck myDeck = new PlayingCards.Deck();
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonDeal_Click(object sender, EventArgs e)
+        {
+            labelOutput.Text = "Displaying a deck of cards delt out in order";
+            int deckIndex = 0;
+                foreach (PictureBox a in this.Controls.OfType<PictureBox>())
+                {
+                    a.Image = myDeck.Cards.ElementAt(deckIndex).FaceImage;
+                    deckIndex += 1;
+                }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            foreach (PlayingCards.CardValues a in Enum.GetValues(typeof(PlayingCards.CardValues)))
+            {
+                PlayingCards.Card clubCard = new PlayingCards.Card(PlayingCards.CardSuits.Club, a);
+                PlayingCards.Card diamondCard = new PlayingCards.Card(PlayingCards.CardSuits.Diamond, a);
+                PlayingCards.Card heartCard = new PlayingCards.Card(PlayingCards.CardSuits.Heart, a);
+                PlayingCards.Card spadeCard = new PlayingCards.Card(PlayingCards.CardSuits.Spade, a);
+
+                myDeck.Cards.Add(clubCard);
+                myDeck.Cards.Add(diamondCard);
+                myDeck.Cards.Add(heartCard);
+                myDeck.Cards.Add(spadeCard);
+            }
+
+            int leftRow2 = 0;
+            int leftShift = 20;
+            for (int i = 0; i <= myDeck.Cards.Count - 1; i++)
+            {
+                if (i == 26)
+                {
+                    leftRow2 = 0;
+                }
+                PictureBox tempPicBox = new PictureBox();
+                tempPicBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                tempPicBox.Height = 100;
+                tempPicBox.Width = 80;
+                tempPicBox.Parent = this;
+                if (i == 0)
+                {
+                    tempPicBox.Left = leftShift;
+                    tempPicBox.Top = 50;
+                }
+                else if (i > 0 && i < 26)
+                {
+                    tempPicBox.Left = leftShift + (i * 20);
+                    tempPicBox.Top = 50;
+                }
+                else if (i >= 26)
+                {
+                    tempPicBox.Left = leftShift + (leftRow2 * 20);
+                    tempPicBox.Top = 180;
+                }
+                leftRow2++;
+            }
+        }
+
+        private void buttonShuffle_Click(object sender, EventArgs e)
+        {
+            labelOutput.Text = "Displaying a deck of cards delt out out in a Random order!";
+            int myRandomNumber = rand.Next(0, myDeck.Cards.Count);
+            PlayingCards.Card tempCard;
+
+            for (int i = 0; i <= myDeck.Cards.Count - 1; i++)           
+            {
+                tempCard = myDeck.Cards[i];
+                myDeck.Cards[i] = myDeck.Cards[myRandomNumber];
+                myDeck.Cards[myRandomNumber] = tempCard;
+                myRandomNumber = rand.Next(0, myDeck.Cards.Count);
+            }
+
+
+            for (int i = 0; i <= myDeck.Cards.Count - 1; i++)
+            {
+                tempCard = myDeck.Cards[i];
+                myDeck.Cards[i] = myDeck.Cards[myRandomNumber];
+                myDeck.Cards[myRandomNumber] = tempCard;
+                myRandomNumber = rand.Next(0, myDeck.Cards.Count);
+            }
+            // re display after shuffling
+            int deckIndex = 0;
+            foreach (PictureBox a in this.Controls.OfType<PictureBox>())
+            {
+                a.Image = myDeck.Cards.ElementAt(deckIndex).FaceImage;
+                deckIndex += 1;
+            }
+
+          
+        }
+
+    }
+}
